bb764f24bab609434c794c20f704b2c8eb75d804
Adarsh <tiwariadarsh0428@gmail.com>
2025-04-17 20:54:34 +0530

    updated password

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URL);
 };
 
-// "mongodb+srv://raghuveerchauhan7787:q5DRcxyO954WNNqb@cluster0.kmiue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 
 module.exports = connectDB;
 
